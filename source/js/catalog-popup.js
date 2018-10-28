a329520631956d552e6bdfc34567f15232f9d183
Kristina <christen.london@gmail.com>
2018-10-28 03:29:29 +0300

    Правкииииииии

--- source/js/catalog-popup.js
+++ source/js/catalog-popup.js
@@ -1,11 +1,11 @@
 var link = document.querySelectorAll('.catalog-main__link');
-var popup = document.querySelector('.catalog-popups__modal');
+var popup = document.querySelector('.catalog-popups--modal');
 var overlay = document.querySelector('.overlay');
 
 for (var i = 0; i < link.length; i++) {
   link[i].addEventListener('click', function (event) {
     event.preventDefault();
-    popup.classList.add('catalog-popups__modal--visible');
+    popup.classList.add('catalog-popups--modal-visible');
     overlay.classList.add('overlay__modal');
   });
 }
@@ -13,8 +13,8 @@ for (var i = 0; i < link.length; i++) {
 window.addEventListener("keydown", function (event) {
   if (event.keyCode === 27) {
     event.preventDefault();
-    if (popup.classList.contains("catalog-popups__modal--visible")) {
-      popup.classList.remove("catalog-popups__modal--visible");
+    if (popup.classList.contains("catalog-popups--modal-visible")) {
+      popup.classList.remove("catalog-popups--modal-visible");
       overlay.classList.remove('overlay__modal');
     }
   }
--- source/js/popup.js
+++ source/js/popup.js
@@ -1,18 +1,18 @@
 var link = document.querySelector('.product-week__link');
-var popup = document.querySelector('.index-popups__modal');
+var popup = document.querySelector('.index-popups--modal');
 var overlay = document.querySelector('.overlay');
 
 link.addEventListener('click', function (event) {
   event.preventDefault();
-  popup.classList.add('index-popups__modal--visible');
+  popup.classList.add('index-popups--modal-visible');
   overlay.classList.add('overlay__modal');
 });
 
 window.addEventListener("keydown", function (event) {
   if (event.keyCode === 27) {
     event.preventDefault();
-    if (popup.classList.contains("index-popups__modal--visible")) {
-      popup.classList.remove("index-popups__modal--visible");
+    if (popup.classList.contains("index-popups--modal-visible")) {
+      popup.classList.remove("index-popups--modal-visible");
       overlay.classList.remove('overlay__modal');
     }
   }
